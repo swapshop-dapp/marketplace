069c4bc4735dfe6eb411a814e0226c357eff2647
Anh Tan Pham <phamanhtan@proton.me>
2024-03-31 16:57:05 +0700

    add base url

--- app/components/ProductItem.tsx
+++ app/components/ProductItem.tsx
@@ -14,46 +14,48 @@ export const ProductItem = () => {
             imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
             imgSrc="https://static.goswapshop.com/images/apple-watch.png"
         >
-            <div className="mb-2 mt-0.5 flex items-center justify-between">
-                {/* eslint-disable-next-line react/jsx-no-undef */}
-                <div className={'flex flex-col'}>
-                    <Avatar img="https://static.goswapshop.com/images/people/profile-picture-5.jpg" rounded bordered
-                            status="online" statusPosition="bottom-right">
-                        <div className="space-y-1 font-medium dark:text-white">
-                            <div >Yang</div>
+            <a href={'/products/1'} target={'_blank'}>
+                
+                <div className="mb-2 mt-0.5 flex items-center justify-between">
+                    {/* eslint-disable-next-line react/jsx-no-undef */}
+                    <div className={'flex flex-col'}>
+                        <Avatar img="https://static.goswapshop.com/images/people/profile-picture-5.jpg" rounded bordered
+                                status="online" statusPosition="bottom-right">
+                            <div className="space-y-1 font-medium dark:text-white">
+                                <div>Yang</div>
+                            </div>
+                            <Rating>
+                                <Rating.Star/>
+                                <Rating.Star/>
+                                <Rating.Star/>
+                                <Rating.Star/>
+                                <Rating.Star filled={false}/>
+                            </Rating>
+                        </Avatar>
+                    </div>
+                    <div>
+                        <span className="text-sm text-gray-600 dark:text-gray-400">12 hours ago</span>
+                        <div className={'flex items-center gap-1 text-white'}>
+                            <FaSafari size={19}/>
+                            <span>Hanoi</span>
                         </div>
-                        <Rating>
-                            <Rating.Star/>
-                            <Rating.Star/>
-                            <Rating.Star/>
-                            <Rating.Star/>
-                            <Rating.Star filled={false}/>
-                        </Rating>
-                    </Avatar>
-                </div>
-                <div>
-                    <span className="text-sm text-gray-600 dark:text-gray-400">12 hours ago</span>
-                    <div className={'flex items-center gap-1 text-white'}>
-                        <FaSafari size={19}/>
-                        <span>Hanoi</span>
                     </div>
                 </div>
-            </div>
-            <a href="#">
                 <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
                     Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
                 </h5>
+                <div className="flex items-center">
+                    <div className={'flex gap-1'}>
+                        <img src="https://static.goswapshop.com/assets/usdt.svg" alt="" width={24}/>
+                        <span className="text-3xl font-bold text-gray-900 dark:text-white">599</span>
+                    </div>
+                    <div className={'items-end self-end align-bottom dark:text-white'}>
+                        <span>~</span>
+                        <span className="text-lg font-bold text-gray-900 dark:text-white">$599</span>
+                    </div>
+                </div>
             </a>
-            <div className="flex items-center">
-                <div className={'flex gap-1'}>
-                    <img src="https://static.goswapshop.com/assets/usdt.svg" alt="" width={24}/>
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">599</span>
-                </div>
-                <div className={'items-end self-end align-bottom dark:text-white'}>
-                    <span>~</span>
-                    <span className="text-lg font-bold text-gray-900 dark:text-white">$599</span>
-                </div>
-            </div>
         </Card>
-    );
+    )
+        ;
 }
